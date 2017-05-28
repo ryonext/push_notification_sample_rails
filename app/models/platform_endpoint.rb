c8f8d55edf9e0847da6ea5d3f856505f70897196
ryonext <ryonext.s@gmail.com>
2017-05-28 18:34:58 +0900

    Fetch platform endpoint from SNS

--- app/models/platform_endpoint.rb
+++ app/models/platform_endpoint.rb
@@ -1,2 +1,13 @@
-class PlatformEndpoint < ApplicationRecord
+class PlatformEndpoint
+  class << self
+    def all
+      client = Aws::SNS::Client.new(region: Settings.region)
+      res = client.list_endpoints_by_platform_application(
+        {
+          platform_application_arn: Settings.platform_application_arn
+        }
+      )
+      res.endpoints
+    end
+  end
 end
